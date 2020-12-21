ccb702a6de89431ef533e0b788e543a1e64e0e6b
Minh Quan <48386342+artiliusjoker@users.noreply.github.com>
2020-12-21 13:30:02 +0700

    add timer, random betrayed nodes

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 //import java.util.Scanner;
+import java.util.Random;
 
 //import org.json.JSONObject;
 import org.json.JSONException;
@@ -10,7 +11,7 @@ import org.json.JSONException;
 public class Main {
 
 	public static void main(String[] args) throws IOException, JSONException {
-		ArrayList<Thread> nodes = new ArrayList<Thread>();
+		ArrayList<Node> nodes = new ArrayList<Node>();
 
 		/* Number node of network */
 		int number = 4;
@@ -20,12 +21,24 @@ public class Main {
 		for (int i = 0; i < number; i++) {
 			ports[i] = 8080 + i;
 			int addport = ports[i];
-			Thread node = new Node(addport);
+			Node node = new Node(addport, i);
 			node.start();
 			nodes.add(node);
 			System.out.println("Node with port number " + addport + " has been added");
 		}
 
+		// Random betrayed nodes
+		Random rand = new Random();
+		// 2 nodes lỗi
+		for(int i = 0; i < 2; ++i)
+		{
+			if(!nodes.get(i).getStatus())
+			{
+				int randomBetrayedIndex = rand.nextInt(number);
+				nodes.get(randomBetrayedIndex).setBetrayed();
+			}	
+		}
+		
 		Node.nodeCount = ports.length;
 		Node.cycle = 0;
 		Sender.ports = ports;
@@ -37,12 +50,35 @@ public class Main {
 		 */
 
 		/* Case2: node 8080 propose message */
-		for (int i = 1; i < ports.length; i++) {
-			Socket socket = new Socket("127.0.0.1", ports[i]);
-			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-			ProposeMessage message = new ProposeMessage(1, 0, (ports[0] - 8080), Integer.toString(ports[0] - 8080));
-			out.println(message.toString());
-			socket.close();
+		for (int i = 0; i < ports.length; i++) {
+//			Socket socket = new Socket("127.0.0.1", ports[i]);
+//			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+//			ProposeMessage message = new ProposeMessage(1, 0, (ports[0] - 8080), Integer.toString(ports[0] - 8080));
+//			out.println(message.toString());
+//			socket.close();
+			
+			nodes.get(i).propose(ports);
+			
+			// Node lỗi gửi không đúng round ngẫu nhiên
+			for(int j = 0; j < ports.length; j ++)
+			{
+				if(nodes.get(j).getStatus() && (j != i))
+				{
+					// Tỉ lệ gửi của node lỗi là 40%
+					if(Math.random() < 0.4)
+					{
+						nodes.get(j).propose(ports);
+					}
+				}
+			}
+			
+			try {
+				Thread.sleep(15 * 1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			Node.cycle += 1;
 		}
 
 	}
--- src/Node.java
+++ src/Node.java
@@ -12,26 +12,30 @@ import java.util.stream.Collectors;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class Node extends Thread {
+public class Node {
 
 	// ArrayList<String> message = new ArrayList<String>();
 	private TreeMap<Integer, Vote> votes = new TreeMap<>();
-
-	int port;
-	ServerSocket server;
+	// Static
 	public static int nodeCount;
 	public static int cycle;
+	
 	private String proposeMessage = null;
+	private final int id;
+	private int port;
+	private ServerSocket server;
+	private boolean isBetrayed = false;
 
-	Node(int port) throws IOException {
+	Node(int port, int id) throws IOException {
 		this.port = port;
 		this.server = new ServerSocket(this.port);
+		this.id = id;
 	}
 
-	public void propose(int[] ports, String proposeMessage) throws UnknownHostException, IOException {
-		
-		this.proposeMessage = proposeMessage;
+	public void propose(int[] ports) throws UnknownHostException, IOException {
 		for (int i = 0; i < ports.length; i++) {
+			if(i == id)
+				continue;
 			Socket socket = new Socket("127.0.0.1", ports[i]);
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 			ProposeMessage message = new ProposeMessage(1, cycle, (port - 8080), Integer.toString(port - 8080));
@@ -85,60 +89,135 @@ public class Node extends Thread {
 
 	}
 
-	@Override
-	public void run() {
-		try {
+//	@Override
+//	public void run() {
+//		try {
+//
+//			int count = 0;
+//			while (true) {
+//				/* Socket to receive incoming requests */
+//				Socket socket = server.accept();
+//				int rePort = socket.getPort();
+//				/* Read message */
+//				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//				String rawMsg = in.readLine();
+//				JSONObject json = new JSONObject(rawMsg);
+//				System.out.println("Node " + Integer.toString(this.port - 8080) + " receive: " + rawMsg);
+//				//System.out.println("Receive port: " + socket.getInetAddress().getHostName()+ " - ID's sender: " + json.getInt("nodeID"));
+//				/* message not match its round */
+//				if (json.getInt("cycle") != cycle) {
+//					continue;
+//				}
+//
+//				count++;
+//
+//				switch (json.getInt("type")) {
+//				case 1: /* receive propose message from proposer */
+//					/* broadcast voting message */
+//					this.proposeMessage = rawMsg;
+//					vote(json, rePort);
+//					break;
+//				case 2: /* receive voting message from another node */
+//					/* Add new vote */
+//					Vote newVote = json.getString("vote").equals("YES") ? Vote.YES : Vote.NO;
+//					votes.put(json.getInt("nodeID"), newVote);
+//
+//					// votes.put(json.getInt("nodeID"), ));
+//					if (count % (nodeCount - 1) == 0) {
+//						/* get majority vote */
+//						Vote vote = getMajorityVote();
+//						if (vote.equals(Vote.YES)) {
+//							/* commit message + write log if majority vote is YES */
+//							commit(this.proposeMessage);
+//						}
+//
+//					}
+//
+//					break;
+//				}
+//
+//				socket.close();
+//				in.close();
+//			}
+//
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+	
+	public void start() {
+		Thread listener = new Thread(new Listener());
+		listener.start();
+	}
+	
+	public void setBetrayed()
+	{
+		isBetrayed = true;
+	}
+	
+	public boolean getStatus()
+	{
+		return isBetrayed;
+	}
+	
+	private class Listener implements Runnable{
 
-			int count = 0;
-			while (true) {
-				/* Socket to receive incoming requests */
-				Socket socket = server.accept();
-				int rePort = socket.getPort();
-				/* Read message */
-				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				String rawMsg = in.readLine();
-				JSONObject json = new JSONObject(rawMsg);
-				System.out.println("Node " + Integer.toString(this.port - 8080) + " receive: " + rawMsg);
-				//System.out.println("Receive port: " + socket.getInetAddress().getHostName()+ " - ID's sender: " + json.getInt("nodeID"));
-				/* message not match its round */
-				if (json.getInt("cycle") != cycle) {
-					continue;
-				}
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			try {
 
-				count++;
-
-				switch (json.getInt("type")) {
-				case 1: /* receive propose message from proposer */
-					/* broadcast voting message */
-					this.proposeMessage = rawMsg;
-					vote(json, rePort);
-					break;
-				case 2: /* receive voting message from another node */
-					/* Add new vote */
-					Vote newVote = json.getString("vote").equals("YES") ? Vote.YES : Vote.NO;
-					votes.put(json.getInt("nodeID"), newVote);
-
-					// votes.put(json.getInt("nodeID"), ));
-					if (count % (nodeCount - 1) == 0) {
-						/* get majority vote */
-						Vote vote = getMajorityVote();
-						if (vote.equals(Vote.YES)) {
-							/* commit message + write log if majority vote is YES */
-							commit(this.proposeMessage);
-						}
-
+				int count = 0;
+				while (true) {
+					/* Socket to receive incoming requests */
+					Socket socket = server.accept();
+					int rePort = socket.getPort();
+					/* Read message */
+					BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+					String rawMsg = in.readLine();
+					JSONObject json = new JSONObject(rawMsg);
+					System.out.println("Node " + Integer.toString(port - 8080) + " receive: " + rawMsg);
+					//System.out.println("Receive port: " + socket.getInetAddress().getHostName()+ " - ID's sender: " + json.getInt("nodeID"));
+					/* message not match its round */
+					if (json.getInt("cycle") != cycle) {
+						continue;
 					}
 
-					break;
+					count++;
+
+					switch (json.getInt("type")) {
+					case 1: /* receive propose message from proposer */
+						/* broadcast voting message */
+						proposeMessage = rawMsg;
+						vote(json, rePort);
+						break;
+					case 2: /* receive voting message from another node */
+						/* Add new vote */
+						Vote newVote = json.getString("vote").equals("YES") ? Vote.YES : Vote.NO;
+						votes.put(json.getInt("nodeID"), newVote);
+
+						// votes.put(json.getInt("nodeID"), ));
+						if (count % (nodeCount - 1) == 0) {
+							/* get majority vote */
+							Vote vote = getMajorityVote();
+							if (vote.equals(Vote.YES)) {
+								/* commit message + write log if majority vote is YES */
+								commit(proposeMessage);
+							}
+
+						}
+
+						break;
+					}
+
+					socket.close();
+					in.close();
 				}
 
-				socket.close();
-				in.close();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
+		
 	}
-
 }
